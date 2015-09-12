05221116e7e7238523a64ee915da838cb6b96edb
Udara Karunarathna <udarapathmin@gmail.com>
2015-09-12 16:28:09 +0530

    navigation

--- application/views/template/navigation.php
+++ application/views/template/navigation.php
@@ -117,11 +117,34 @@
                     <li class="active">
                         <a href="<?php echo base_url("index.php/Welcome/"); ?>"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
                     </li>
-                     <li>
-                        <a href="users.php"><i class="fa fa-fw fa-bar-chart-o"></i> Manage Freebies</a>
+                    <li>
+                        <a href='<?php echo base_url("index.php/treatments"); ?>'><i class="fa fa-fw fa-stethoscope"></i> Treatments Management</a>
                     </li>
-                        <li>
+
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-github-alt"></i> Animal Management <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="demo" class="collapse">
+                            <li>
+                                <a href="<?php echo base_url("index.php/animal/"); ?>">Animal Management</a>
+                            </li>
+                            <li>
+                                <a href="<?php echo base_url("index.php/animal/animal_cat"); ?>">Animal Category</a>
+                            </li>
+                            <li>
+                                <a href="<?php echo base_url("index.php/animal/location"); ?>">Animal Location</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                   <!--  <li>
                         <a href="offers.php"><i class="fa fa-fw fa-wrench"></i> Manage Offers</a>
+                    </li> -->
+                    <li>
+                        <a href='<?php echo base_url("index.php/show_management"); ?>'><i class="fa fa-fw fa-ticket"></i> Shows Management</a>
+                    </li>
+
+                    <li>
+                        <a href='<?php echo base_url("index.php/shows"); ?>'><i class="fa fa-fw fa-film"></i> Current Shows</a>
                     </li>
                     
                     
@@ -143,17 +166,6 @@
                         </ul>
                     </li>
                     
-                    <li>
-                        <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i>etc Dropdown <i class="fa fa-fw fa-caret-down"></i></a>
-                        <ul id="demo" class="collapse">
-                            <li>
-                                <a href="#">Dropdown Item</a>
-                            </li>
-                            <li>
-                                <a href="#">Dropdown Item</a>
-                            </li>
-                        </ul>
-                    </li>
                     <li>
                         <a href="<?php echo base_url("index.php/User_Auth/logout"); ?>"><i class="fa fa-fw fa-power-off"></i> Logout</a>
                     </li>
